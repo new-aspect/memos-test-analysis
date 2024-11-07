b37eddb930d3db321175893ac88878fa062b7591
nzhao <nzhao@sudytech.com>
2024-11-07 13:40:51 +0800

    feat 完成oauth2的测试案例

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module new-aspect/memos-test-analysis
 go 1.23.2
 
 require (
+	github.com/Jeffail/tunny v0.1.4 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
--- plugin/idp/oauth2/oauth2_test.go
+++ plugin/idp/oauth2/oauth2_test.go
@@ -1,7 +1,16 @@
 package oauth2
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"new-aspect/memos-test-analysis/plugin/idp"
 	"new-aspect/memos-test-analysis/store"
 	"testing"
 )
@@ -64,7 +73,95 @@ func TestNewIdentityProvider(t *testing.T) {
 	}
 }
 
-// 测试整体的逻辑，包含用没有直接访问权限的授权码
-func TestIdentityProvider(t *testing.T) {
+// 模拟oauth 服务器
+// 这个服务器提供两个接口，一个是 /oauth2/token 将授权码Code转换为accessToken
+// 另一个是 /oauth2/userinfo 返回用户信息
+func newMockServer(t *testing.T, testCoke, testAccessToken string, userInfo []byte) *httptest.Server {
+	mux := http.NewServeMux()
 
+	mux.HandleFunc("/oauth2/token", func(w http.ResponseWriter, r *http.Request) {
+		// oauth2/token 首先取出来授权码code, 然后判断这个code是否正确
+		// 这个是一个POST请求,
+		require.Equal(t, http.MethodPost, r.Method)
+
+		body, err := io.ReadAll(r.Body)
+		require.NoError(t, err)
+		vals, err := url.ParseQuery(string(body))
+		require.NoError(t, err)
+
+		// 如果code正确，则返回一个accessToken
+		var rawIDToken string
+		require.Equal(t, testCoke, vals.Get("code"))
+		require.Equal(t, "authorization_code", vals.Get("grant_type"))
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]any{
+			"access_token":  testAccessToken,
+			"token_type":    "Bearer",
+			"refresh_token": "test-refresh-token",
+			"expires_in":    3600,
+			"id_token":      rawIDToken,
+		})
+		require.NoError(t, err)
+	})
+	mux.HandleFunc("/oauth2/userinfo", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		_, err := w.Write(userInfo)
+		require.NoError(t, err)
+	})
+
+	return httptest.NewServer(mux)
+}
+
+// 测试整体的逻辑，包含用没有直接访问权限的授权码兑换成有访问权限的token,然后根据token访问用户信息
+// 这个过程中需要模拟oauth服务器实现着2次交互，
+// 先写发起请求的这2个过程，就是先调用ExchangeToken将授权码兑换成为token
+func TestIdentityProvider(t *testing.T) {
+	const (
+		testClientID    = "test-client-id"
+		testCode        = "test-code"
+		TestAccessToken = "test-access-token"
+		testSubject     = "123456789"
+		testName        = "Ask Ning"
+		testEmail       = "ask.ning@example.com"
+	)
+
+	userInfo, err := json.Marshal(
+		map[string]any{
+			"sub":   testSubject,
+			"name":  testName,
+			"email": testEmail,
+		},
+	)
+	require.NoError(t, err)
+
+	s := newMockServer(t, testCode, TestAccessToken, userInfo)
+
+	provider, err := NewIdentityProvider(&store.IdentityProviderOAuth2Config{
+		ClientID:     testClientID,
+		ClientSecret: testCode,
+		TokenURL:     fmt.Sprintf("%s/oauth2/token", s.URL),
+		UserInfoURL:  fmt.Sprintf("%s/oauth2/userinfo", s.URL),
+		FieldMapping: &store.FieldMapping{
+			Identifier:  "sub",
+			DisplayName: "name",
+			Email:       "email",
+		},
+	})
+	require.NoError(t, err)
+
+	redirectURL := "https://example.com/oauth/callback"
+	token, err := provider.ExchangeToken(context.Background(), redirectURL, testCode)
+	require.NoError(t, err)
+
+	userInfoResult, err := provider.UserInfo(token)
+	require.NoError(t, err)
+
+	// 判断userInfo与我们想要得到的一致
+	wantUserInfo := &idp.IdentityProviderUserInfo{
+		Identifier:  testSubject,
+		DisplayName: testName,
+		Email:       testEmail,
+	}
+	require.Equal(t, userInfoResult, wantUserInfo)
 }
